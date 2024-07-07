d60d73db506f9e7cc05ecd27c71815656e71b79b
Wu-chang Feng <wuchang@pdx.edu>
2024-07-07 15:30:43 -0700

    move to langchain

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,42 @@
-# Use a slim Python base layer
-FROM python:3.9-slim
+# Multi-stage build.  To build the container, you will need to supply your
+# GOOGLE_API_KEY to load the vector database within the final container
+#   docker build --build-arg GOOGLE_API_KEY=$GOOGLE_API_KEY -f Dockerfile -t wuchangfeng/pdx-cs-ask .
 
-# Specify your e-mail address as the maintainer of the container image
-LABEL maintainer="wuchang@pdx.edu"
+# Use an official Python runtime as a parent image
+FROM python:3.10-slim as builder
 
-# Copy the contents of the current directory into the container directory /app
-COPY . /app
+# Ingestion script needs GoogleAI embeddings to insert into vector database.
+# Pull GOOGLE_API_KEY from arguments
+ARG GOOGLE_API_KEY
 
-# Set the working directory of the container to /app
+# Set environment variable for ingestion script
+ENV GOOGLE_API_KEY=$GOOGLE_API_KEY
+
+# Set the working directory in the container
 WORKDIR /app
 
-# Create a virtual environment
-RUN python3 -m venv env
+# Copy the current directory contents into the container at /app
+COPY . /app
 
-# Install the Python packages specified by requirements.txt into the container
-RUN env/bin/pip install -r requirements.txt
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
-# Set the parameters to the program
-CMD env/bin/gunicorn --bind :$PORT --workers 1 --threads 8 app:app
+# Run ingestion script to populate vector database
+RUN python loaddb.py
+
+# Use an official Python runtime as a parent image
+FROM python:3.10-slim
+
+# Copy application over
+COPY --from=builder /app /app
+
+# Copy package libraries over
+COPY --from=builder /usr/local/lib/python3.10/site-packages/ /usr/local/lib/python3.10/site-packages/
+
+# Set working directory
+WORKDIR /app
+
+# Launch gunicorn
+CMD gunicorn --bind :$PORT --workers 1 --threads 8 app:app
+
+#CMD python3 gunicorn --bind :$PORT --workers 1 --threads 8 app:app
--- query.py
+++ query.py
@@ -1,6 +1,17 @@
 from flask import redirect, request, url_for, render_template, session
 from flask.views import MethodView
-from llm import myllm
+from langchain import hub
+from langchain_community.vectorstores import Chroma
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+import re
+
+def format_docs(docs):
+    output = "\n\n".join(doc.page_content for doc in docs)
+    sources = {doc.metadata['source'] for doc in docs}
+    source_list = "\nSource: ".join(source for source in sources)
+    return output+source_list
 
 class Query(MethodView):
     def get(self):
@@ -11,5 +22,33 @@ class Query(MethodView):
         Accepts POST requests, and processes the form;
         Redirect to index when completed.
         """
-        answer = myllm.query(request.form['message'])
+        vectorstore = Chroma(
+                embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001", task_type="retrieval_query"),
+                persist_directory="./.chromadb"
+        )
+        llm = GoogleGenerativeAI(model="gemini-1.5-flash")
+        retriever = self.vectorstore.as_retriever()
+
+        prompt_template = """You are an assistant for question-answering tasks. Use the following context to answer the question.  Provide the source URLs of the context you used to perform the task and instruct the user to visit them for more information.  If you don't know the answer, just say that you don't know.
+
+Question: {question}
+
+Context: {context}
+
+Answer: """
+
+        # create a prompt example from above template
+        prompt = PromptTemplate(
+            input_variables=["question"],
+            template=prompt_template
+        )
+ 
+        rag_chain = (
+            {"context": retriever | format_docs, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+
+        answer = rag_chain.invoke(request.form['message'])
         return render_template('query.html',question=request.form['message'],answer=answer)
